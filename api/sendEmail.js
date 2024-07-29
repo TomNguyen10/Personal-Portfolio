5c1054b4f482a6e18ea46c634c597a6a52eccc39
Tom Nguyen <dungnguyen_2025@depauw.edu>
2024-07-28 22:53:49 -0600

    FIX: Change to nodemailer

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,21 +1,28 @@
-const Resend = require("resend");
-const { RESEND_API_KEY } = process.env;
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const resend = new Resend(RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 module.exports = async (req, res) => {
   if (req.method === "POST") {
     const { name, email, message } = req.body;
     console.log("Form Data:", { name, email, message });
 
-    try {
-      await resend.emails.send({
-        from: "dungnguyen_2025@depauw.edu",
-        to: "dungnguyen_2025@depauw.edu",
-        subject: `New contact form submission from ${name}`,
-        text: `You have received a new message from ${name} (${email}):\n\n${message}`,
-      });
+    const mailOptions = {
+      from: "dungnguyen_2025@depauw.edu",
+      to: "dungnguyen_2025@depauw.edu",
+      subject: `New contact form submission from ${name}`,
+      text: `You have received a new message from ${name} (${email}):\n\n${message}`,
+    };
 
+    try {
+      await transporter.sendMail(mailOptions);
       console.log("Email sent successfully");
       res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
